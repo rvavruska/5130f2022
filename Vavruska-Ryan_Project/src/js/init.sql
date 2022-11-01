09b37afc33944c9b87891397843da7b1005e0b29
rvavruska <rvavruska@gmail.com>
2022-11-01 19:53:45 -0400

    add some project files

---
+++ Vavruska-Ryan_Project/src/js/init.sql
@@ -0,0 +1,24 @@
+create_table users (
+	id int,
+    user_name varchar(20),
+    password varchar(60),
+    first_name varchar(20),
+	last_name varchar(20),
+	primary key (id)	
+);
+
+create_table products (
+	id int,
+	product_name varchar(20),
+	url varchar(20),
+	primary key (id)
+);
+
+create_table subscriptions (
+	s_id int,
+	u_id int,
+	p_id int,
+	primary key(s_id),
+	foreign key (u_id) references users(u_id),
+	foreign key (u_id) references products(p_id),
+);
