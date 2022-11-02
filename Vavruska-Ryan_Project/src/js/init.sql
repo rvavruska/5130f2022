623ff5b23283bbd3452ff8cf40e1b0bfe744bd71
rvavruska <rvavruska@gmail.com>
2022-11-01 20:37:50 -0400

    add base html pages

--- Vavruska-Ryan_Project/src/js/init.sql
+++ Vavruska-Ryan_Project/src/js/init.sql
@@ -2,6 +2,7 @@ create_table users (
 	id int,
     user_name varchar(20),
     password varchar(60),
+	email varchar(60),
     first_name varchar(20),
 	last_name varchar(20),
 	primary key (id)	
@@ -18,6 +19,8 @@ create_table subscriptions (
 	s_id int,
 	u_id int,
 	p_id int,
+	start_date DATE,
+	renewal_period int,
 	primary key(s_id),
 	foreign key (u_id) references users(u_id),
 	foreign key (u_id) references products(p_id),
