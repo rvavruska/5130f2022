5288e1cf1aeb964bbbdd6b912a3e4a9339f339a4
Ryan Vavruska <rvavruska@gmail.com>
2022-11-21 13:29:15 -0500

    update unsubscribe filename

---
+++ Vavruska-Ryan_Project/subtracker/src/Unsubscribe.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+import { createRoutesFromElements } from "react-router-dom";
+import "./Unsubscribe.css";
+
+function Unsubscribe({product}) {
+
+    return (
+        <h1>{product}</h1>
+    )
+}
+export default Unsubscribe;
